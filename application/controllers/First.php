bf512f859bb5743a0dbe98351f82ee189c98c58e
s-rebecca-fu <rebeccafu1991@gmail.com>
2017-02-02 00:45:19 -0500

    first controller modified, and add a first method in Quotes

--- application/controllers/First.php
+++ application/controllers/First.php
@@ -4,29 +4,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class First extends Application
 {
-
+	
 	/**
-	 * Homepage for our app
+	 * First for our app
 	 */
 	public function index()
 	{
 		// this is the view we want shown
-		$this->data['pagebody'] = 'homepage';
+		$this->data['pagebody'] = 'justone';
+		
 		// build the list of authors, to pass on to our view
-		$source = $this->quotes->all();
-		$authors = array ();
-		
-		foreach ($source as $record)
-		{
-			$authors[] = array ('who' => $record['who'], 'mug' => $record['mug'], 'href' => $record['where'], 'what' =>  $record['what']);
-		}
-		
-		//$rand_keys = rand(0, sizeof($authors)-1);
-		
-		$result = array($authors[0]);
-		
-		$this->data['authors'] = $result;
+		$source = $this->quotes->first();
+		$this->data = array_merge($this->data, $source);
 		$this->render();
+	
 	}
 
 }
